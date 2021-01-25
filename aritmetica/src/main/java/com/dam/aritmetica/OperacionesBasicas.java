c8fb440749fda9a11699bbe81e40fa51b795591c
eloyvillar <77961582+eloyvillar@users.noreply.github.com>
2021-01-25 10:15:20 +0100

    Add files via upload

---
+++ aritmetica/src/main/java/com/dam/aritmetica/OperacionesBasicas.java
@@ -0,0 +1,36 @@
+package com.dam.aritmetica;
+
+public class OperacionesBasicas {
+
+	public static int suma(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int resta(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public static int multiplicacion(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public static int division(int a, int b)
+	{
+		return a/b;
+	}
+	public static int raizCuadrada(int a)
+	{
+		return a*a;
+	}
+	public static int areaCirculo(int a, int b)
+	{
+		return (314*a)/2;
+	}	
+	public static int areaTriangulo(int a, int b)
+	{
+		return (a*b)/2;
+	}
+}
